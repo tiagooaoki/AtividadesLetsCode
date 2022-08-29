e33596b3921abf34ad306e45fe8d8c8bf8520ac9
Tiago Aoki <tiagoaoki@gmail.com>
2022-08-29 20:54:43 -0300

    criado logs aos endpoints

--- src/main/java/com/letscode/ecommerce/endpoints/HelloEndpoints.java
+++ src/main/java/com/letscode/ecommerce/endpoints/HelloEndpoints.java
@@ -1,5 +1,7 @@
 package com.letscode.ecommerce.endpoints;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,21 +14,36 @@ import com.letscode.ecommerce.services.HelloService;
 
 @RestController
 public class HelloEndpoints {
-
+    
+    Logger logger = LoggerFactory.getLogger(HelloEndpoints.class);
     @Autowired
     HelloService helloServices;
 
+    @RequestMapping("/")
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+
     @RequestMapping(path = "/hello", method = RequestMethod.GET)
     public ResponseEntity<String> hello(){
+        logger.info("INFO: Este endpoint vem com a mensagem de Hello");
         return new ResponseEntity<String>("Hello world!", HttpStatus.OK);
     }
 
     @RequestMapping(path = "/hello/{nome}", method = RequestMethod.GET)
     public ResponseEntity<String> helloPerson(@PathVariable String nome){
+        logger.info("INFO: Este endpoint vem com a mensagem de Hello + nome");
         return new ResponseEntity<String>("Hello "+ nome, HttpStatus.OK);
     }
     @RequestMapping(path = "/hello/{nome}/horario", method = RequestMethod.GET)
     public ResponseEntity helloPersonTime(@PathVariable String nome){
+        logger.info("INFO: Este endpoint vem com a mensagem de Hello + nome + a hora");
         return new ResponseEntity<>("Hello "+ nome + "! Agora são " + helloServices.helloComHorario() + ", não esqueça!", HttpStatus.OK);
     }
     
